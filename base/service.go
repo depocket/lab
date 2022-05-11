c88662e07208b6aec6c55f50db31228122b71bb7
Himitsuko <himitsuko@github.com>
2022-05-11 17:49:00 +0700

    add: Base platform integration

---
+++ base/service.go
@@ -0,0 +1,63 @@
+package base
+
+import (
+	"github.com/depocket/studio/sdk"
+	"math/big"
+)
+
+type BalanceGroup struct {
+	Type  string `json:"type"`
+	Label string `json:"label"`
+}
+
+type Balance struct {
+	Group BalanceGroup `json:"group"`
+	Pools []Pool       `json:"pools"`
+}
+
+type Pool struct {
+	Name         string         `json:"name"`
+	Apy          *big.Float     `json:"apy"`
+	RewardTokens []TokenBalance `json:"reward_tokens"`
+	SupplyTokens []TokenBalance `json:"supply_tokens"`
+	BorrowTokens []TokenBalance `json:"borrow_tokens"`
+}
+
+type TokenBalance struct {
+	Address  string     `json:"address"`
+	Balance  *big.Float `json:"balance"`
+	Symbol   string     `json:"symbol"`
+	IconUrl  string     `json:"icon_url"`
+	Price    float64    `json:"price"`
+	Chain    string     `json:"chain"`
+	Decimals uint64     `json:"decimals"`
+	Name     string     `json:"name"`
+}
+
+type AppManifest struct {
+	Code              string         `json:"code"`
+	Name              string         `json:"name"`
+	Tags              []string       `json:"tags"`
+	Description       string         `json:"description"`
+	Url               string         `json:"url"`
+	Links             []string       `json:"links"`
+	SupportedNetworks []string       `json:"supported_networks"`
+	Groups            []BalanceGroup `json:"groups"`
+}
+
+type AppService interface {
+	Code() string
+	Name() string
+	Tags() []string
+	Description() string
+	Url() string
+	Links() map[string]string
+	SupportedNetworks() []string
+	Groups() []BalanceGroup
+
+	FetchBalances([]sdk.Token) ([]Balance, error)
+	FetchStakingBalance([]sdk.Token) (Balance, error)
+	FetchFarmingBalance([]sdk.Token) (Balance, error)
+	FetchLendingBalance([]sdk.Token) (Balance, error)
+	FetchBorrowBalance([]sdk.Token) (Balance, error)
+}
