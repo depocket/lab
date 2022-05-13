0af180bf34e05639e5fc10e08bbac4122b82eb02
Himitsuko <himitsuko@github.com>
2022-05-13 16:21:50 +0700

    add: Sample CLI codegen

--- cli/codegen.go
+++ cli/codegen.go
@@ -1,1 +1,11 @@
-package cli
+package main
+
+import (
+	"github.com/depocket/lab/cli/templates"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.New(templates.DAppsIntegrationTemplate)
+	tpl.Option()
+}
